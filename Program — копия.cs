0d923f2dbaf2e80901a18e14c29c2daf9e55b57b
NiKiTaGitHub1111 <103022224+NiKiTaGitHub1111@users.noreply.github.com>
2022-04-05 09:33:28 +0300

    Add files via upload

---
+++ Program — копия.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Арбуз
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет ягодам");
+            Console.ReadLine();
+            Console.WriteLine("Привет Арбуз");
+            Console.ReadLine();
+        }
+    }
+}
